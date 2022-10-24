c69cc5e8ab6b4fa8dc5d94fd99df4486684f66b1
João Penim <38078990+joao-penim@users.noreply.github.com>
2022-10-24 22:12:44 +0100

    Scala FMT init (#5) (#7)

--- src/main/scala/models/ParserInput.scala
+++ src/main/scala/models/ParserInput.scala
@@ -3,6 +3,8 @@ package models
 import formats.CSVDefinition
 import parsing.Parser.EncodeType
 
-case class ParserInput(in: SupportedType,
-                       csvDefinition: CSVDefinition,
-                       headers: List[String] = Nil)(implicit encode: EncodeType)
+case class ParserInput(
+    in: SupportedType,
+    csvDefinition: CSVDefinition,
+    headers: List[String] = Nil
+)(implicit encode: EncodeType)
