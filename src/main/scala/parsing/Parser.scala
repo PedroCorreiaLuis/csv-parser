58fa036af7ad1aff1c10301495c92fa3ada7c14c
Ricardo Chaves <96072579+ricardochaves9@users.noreply.github.com>
2022-11-02 00:29:53 +0000

    Reader type fix  (#27)

--- src/main/scala/parsing/Parser.scala
+++ src/main/scala/parsing/Parser.scala
@@ -1,8 +1,9 @@
 package parsing
 
+import formats.CSV.{delimiter, terminator}
 import models._
-
 import scala.io.Source
+
 
 object Parser extends Encoding {
 
@@ -36,20 +37,20 @@ object Parser extends Encoding {
           parsedLines = droppedLines.map(_.merge),
           droppedLines = parsedLines.map(_.merge)
         )
-      case ReaderType(input)   =>
-        val source: Source =  Source.fromString(input.toString)
-        val sourceLines: Iterator[String] = source.getLines()
-        val parsed: Seq[Either[EncodeType, EncodeType]] = sourceLines.toList.map(parsingLogic)
-        source.close()
+      case ReaderType(input) =>
+        val iterator = Iterator.continually(input.read().toChar).toArray
+        val lines  = Source.fromChars(iterator)
+        val parsed = lines.getLines().map(parsingLogic)
+        lines.close()
         val (
-          droppedLines: List[Either[String, String]],
-          parsedLines: List[Either[String, String]]
-        ) = parsed.partition(_.isLeft)
+          droppedLines: Iterator[Either[String, String]],
+          parsedLines: Iterator[Either[String, String]]
+          ) = parsed.partition(_.isLeft)
 
         ParsedCSV(
           headers = parserInput.headers,
-          parsedLines = droppedLines.map(_.merge),
-          droppedLines = parsedLines.map(_.merge)
+          parsedLines = parsedLines.map(_.merge).toList,
+          droppedLines = droppedLines.map(_.merge).toList
         )
 
       case SeqType(input) =>
@@ -127,17 +128,16 @@ object Parser extends Encoding {
               headers = headers.toList
             )
           case ReaderType(input) =>
-            val source: Source = Source.fromString(input.toString)
-            val sourceLines: Iterator[String] = source.getLines()
-            val headers: Seq[EncodeType] = sourceLines.take(1).toList
-            val lines = sourceLines
+            val iterator: Array[Char] = Iterator.continually(input.read().toChar).toArray
+            val headers: Seq[EncodeType] = iterator.takeWhile(_ != terminator).toList.mkString.split(delimiter).toList
+            val source = Source.fromChars(iterator)
+            val lines = source.getLines()
             source.close()
             ParserInput(
               in = IteratorType(lines),
               csvDefinition = parserInput.csvDefinition,
               headers = headers.toList
             )
-
           case SeqType(input) =>
             val headers: List[String] = input.take(1).toList
             val lines: Seq[String] = input.drop(1)
@@ -146,7 +146,6 @@ object Parser extends Encoding {
               csvDefinition = parserInput.csvDefinition,
               headers = headers
             )
-
           case SourceType(input) =>
             val headers: Iterator[String] = input.getLines().take(1)
             val lines: Iterator[String] = input.getLines()
